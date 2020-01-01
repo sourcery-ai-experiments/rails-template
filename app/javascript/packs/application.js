9df7fc265d1fa3cd4c50995ab8d6bc71c4b1bc1d
Muhammad Soban Akram <soban.se@gmail.com>
2020-01-01 15:15:24 +0500

    core ui added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("jquery");
+require("perfect-scrollbar");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +18,6 @@ const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
 import '../stylesheets/application'
+
+import '@coreui/coreui';
+// import '@coreui/coreui/dist/js/coreui-utilities';
